16bc88137dc58df5e8e8389758acac7a2b944d52
steven <1991585851@qq.com>
2017-12-10 14:55:36 +0800

    Update related.py

--- django/db/models/fields/related.py
+++ django/db/models/fields/related.py
@@ -781,7 +781,7 @@ class ForeignKey(ForeignObject):
     }
     description = _("Foreign Key (type determined by related field)")
 
-    def __init__(self, to, on_delete, related_name=None, related_query_name=None,
+    def __init__(self, to, on_delete=None, related_name=None, related_query_name=None,
                  limit_choices_to=None, parent_link=False, to_field=None,
                  db_constraint=True, **kwargs):
         try:
